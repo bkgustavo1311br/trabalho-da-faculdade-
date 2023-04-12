45300e4772cb5512424047dab6172248ff9e7354
Guts <130602126+bkgustavo1311br@users.noreply.github.com>
2023-04-12 17:33:53 -0400

    Add files via upload

---
+++ questao8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char estado[3];
+    printf("Digite a sigla do estado: ");
+    scanf("%s", estado);
+    
+    if (estado[0] == 'A' && estado[1] == 'M') {
+        printf("Você é Amazonense\n");
+    } else if (estado[0] == 'R' && estado[1] == 'J') {
+        printf("Você é Carioca\n");
+    } else if (estado[0] == 'P' && estado[1] == 'A') {
+        printf("Você é Paraense\n");
+    } else if (estado[0] == 'A' && estado[1] == 'C') {
+        printf("Você é Acreano\n");
+    } else {
+        printf("Não foi possível identificar o estado\n");
+    }
+    
+    return 0;
+}
