5c9448407fd142d158ff35ed7a5d9feec57fdba0
Guts <130602126+bkgustavo1311br@users.noreply.github.com>
2023-04-15 23:44:41 -0300

    Update questao6.c

--- questao6.c
+++ questao6.c
@@ -3,17 +3,14 @@
 int main() {
     float diagonalMaior, diagonalMenor, area;
 
-    // Ler as diagonais do losango
     printf("Digite o valor da diagonal maior: ");
     scanf("%f", &diagonalMaior);
     printf("Digite o valor da diagonal menor: ");
     scanf("%f", &diagonalMenor);
 
-    // Calcular a área do losango usando a fórmula
     area = diagonalMaior * diagonalMenor / 2;
 
-    // Exibir o resultado
     printf("A area do losango e: %.2f\n", area);
 
-    return 0;
+    return 0;
 }
