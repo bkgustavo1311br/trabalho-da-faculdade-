7309173794524111ea51d2c13d0a051ef53ef25d
Guts <130602126+bkgustavo1311br@users.noreply.github.com>
2023-04-16 00:19:00 -0300

    Update questao 2.cpp

--- questao 2.cpp
+++ questao 2.cpp
@@ -1,24 +1,25 @@
-	#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+
 int main() {
-  char nome[50], nota_str[10];
-  double nota1, nota2, media;
+char nome[50], nota_str[10];
+double nota1, nota2, media;
 
-  printf("Digite o nome do aluno: ");
-  scanf("%s", nome);
+printf("Digite o nome do aluno: ");
+scanf("%s", nome);
 
-  printf("Digite a primeira nota: ");
-  scanf("%s", nota_str);
-  nota1 = strtod(nota_str, NULL);
+printf("Digite a primeira nota: ");
+scanf("%s", nota_str);
+nota1 = strtod(nota_str, NULL);
 
-  printf("Digite a segunda nota: ");
-  scanf("%s", nota_str);
-  nota2 = strtod(nota_str, NULL);
+printf("Digite a segunda nota: ");
+scanf("%s", nota_str);
+nota2 = strtod(nota_str, NULL);
 
-  media = (nota1 + nota2) / 2;
+media = (nota1 + nota2) / 2;
 
-  printf("A nota do aluno %s é %.2f", nome, media);
+printf("A nota do aluno %s Ã© %.2f\n", nome, media);
 
-  return 0;
+return 0;
 }
